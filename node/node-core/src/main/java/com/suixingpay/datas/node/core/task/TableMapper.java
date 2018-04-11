55adaf3e169c5c79814020e6e6443cfb229b5119
张科伟 <zhang_kw@suixingpay.com>
2018-04-11 09:34:31 +0800

    不符合checkstyle代码修改

--- node/node-core/src/main/java/com/suixingpay/datas/node/core/task/TableMapper.java
+++ node/node-core/src/main/java/com/suixingpay/datas/node/core/task/TableMapper.java
@@ -10,8 +10,6 @@
 package com.suixingpay.datas.node.core.task;
 
 import com.suixingpay.datas.common.config.TableMapperConfig;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -107,7 +105,7 @@ public class TableMapper {
 
         if (null != column) {
             Arrays.stream(column.keySet().toArray(new String[0])).forEach(k -> {
-                String v = ignoreTargetCase ? column.get(k). toLowerCase() : column.get(k);
+                String v = ignoreTargetCase ? column.get(k).toLowerCase() : column.get(k);
                 column.remove(k);
                 column.put(k.toUpperCase(), v);
             });
