de8f993e3233e9b9b2d73788ce9ac03f59166af5
guohongjian <guo_hj@suixingpay.com>
2018-04-12 14:06:32 +0800

    config push

--- manager/manager-boot/src/main/java/com/suixingpay/datas/manager/controller/NodesController.java
+++ manager/manager-boot/src/main/java/com/suixingpay/datas/manager/controller/NodesController.java
@@ -45,16 +45,16 @@ public class NodesController {
      * @author FuZizheng
      * @date 2018/3/16 下午3:34
      * @param: [pageNo,
-     * pageSize, ipAddress, state, machineName, type]
+     *             pageSize, ipAddress, state, machineName, type]
      * @return: com.suixingpay.datas.manager.web.message.ResponseMessage
      */
     @GetMapping
     @ApiOperation(value = "查询列表", notes = "查询列表")
     public ResponseMessage list(@RequestParam(value = "pageNo", required = true) Integer pageNo,
-                                @RequestParam(value = "pageSize", required = true) Integer pageSize,
-                                @RequestParam(value = "ipAddress", required = false) String ipAddress,
-                                @RequestParam(value = "state", required = false) Integer state,
-                                @RequestParam(value = "machineName", required = false) String machineName) {
+            @RequestParam(value = "pageSize", required = true) Integer pageSize,
+            @RequestParam(value = "ipAddress", required = false) String ipAddress,
+            @RequestParam(value = "state", required = false) Integer state,
+            @RequestParam(value = "machineName", required = false) String machineName) {
         Page<Nodes> page = nodesService.page(new Page<Nodes>(pageNo, pageSize), ipAddress, state, machineName);
         return ok(page);
     }
@@ -91,16 +91,15 @@ public class NodesController {
     @PostMapping("/taskpushstate")
     @ApiOperation(value = "任务状态推送", notes = "任务状态推送")
     public ResponseMessage taskPushState(@RequestParam(value = "id", required = true) Long id,
-                                         @RequestParam(value = "taskPushState", required = true) NodeStatusType taskPushState) throws Exception {
+            @RequestParam(value = "taskPushState", required = true) NodeStatusType taskPushState) throws Exception {
         Integer i = nodesService.taskPushState(id, taskPushState);
-        // if (i == 1) {
-        // System.out.println("推送任务 运行中|暂停:" + id);
-        Nodes nodes = nodesService.selectById(id);
-        ClusterProviderProxy.INSTANCE.broadcast(new NodeOrderPushCommand(
-                new NodeCommandConfig(nodes.getNodeId().toString(), taskPushState, NodeCommandType.CHANGE_STATUS)));
-        return ok(nodes);
-        // }
-        // return ok(false);
+        if (i == 1) {
+            Nodes nodes = nodesService.selectById(id);
+            ClusterProviderProxy.INSTANCE.broadcast(new NodeOrderPushCommand(
+                    new NodeCommandConfig(nodes.getNodeId().toString(), taskPushState, NodeCommandType.CHANGE_STATUS)));
+            return ok(nodes);
+        }
+        return ok(false);
     }
 
     @PostMapping("/stoptask")
@@ -108,8 +107,9 @@ public class NodesController {
     public ResponseMessage stopTask(@RequestParam(value = "id", required = true) Long id) throws Exception {
         // System.out.println("停止任务:" + id);
         Nodes nodes = nodesService.selectById(id);
-        ClusterProviderProxy.INSTANCE.broadcast(new NodeOrderPushCommand(
-                new NodeCommandConfig(nodes.getNodeId().toString(), NodeStatusType.SUSPEND, NodeCommandType.RELEASE_WORK)));
+        ClusterProviderProxy.INSTANCE
+                .broadcast(new NodeOrderPushCommand(new NodeCommandConfig(nodes.getNodeId().toString(),
+                        NodeStatusType.SUSPEND, NodeCommandType.RELEASE_WORK)));
         return ok();
     }
 }
