ccae90fa416d9da76b89cb8cc7fd7bf163821b2b
guohongjian <guo_hj@suixingpay.com>
2018-04-16 11:22:20 +0800

    heat date.

--- manager/manager-boot/src/main/java/com/suixingpay/datas/manager/service/impl/CUserServiceImpl.java
+++ manager/manager-boot/src/main/java/com/suixingpay/datas/manager/service/impl/CUserServiceImpl.java
@@ -3,19 +3,15 @@
  */
 package com.suixingpay.datas.manager.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.suixingpay.datas.manager.core.entity.CUser;
 import com.suixingpay.datas.manager.core.mapper.CUserMapper;
 import com.suixingpay.datas.manager.service.CUserService;
 import com.suixingpay.datas.manager.web.page.Page;
-import org.apache.tomcat.util.security.MD5Encoder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import sun.misc.BASE64Encoder;
-
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
 
 /**
  * 登陆用户表 服务实现类
