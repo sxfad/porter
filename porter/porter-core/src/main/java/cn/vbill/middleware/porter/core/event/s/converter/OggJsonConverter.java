57d61d3ac75e6b0876c9299584acbdf8b73b1113
Fuzz <fu_zz@Suixingpay.com>
2018-08-03 13:53:50 +0800

    优化import

--- porter/porter-core/src/main/java/cn/vbill/middleware/porter/core/event/s/converter/OggJsonConverter.java
+++ porter/porter-core/src/main/java/cn/vbill/middleware/porter/core/event/s/converter/OggJsonConverter.java
@@ -17,16 +17,14 @@
 
 package cn.vbill.middleware.porter.core.event.s.converter;
 
+import cn.vbill.middleware.porter.common.consumer.Position;
+import cn.vbill.middleware.porter.common.dic.ConsumeConverterPlugin;
 import cn.vbill.middleware.porter.core.event.s.EventConverter;
 import cn.vbill.middleware.porter.core.event.s.EventType;
 import cn.vbill.middleware.porter.core.event.s.MessageEvent;
-import cn.vbill.middleware.porter.common.dic.ConsumeConverterPlugin;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import cn.vbill.middleware.porter.common.consumer.Position;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
