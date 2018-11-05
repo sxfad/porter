8e9805c59a97ef99404e7bbff5e3a10493fd6810
zhangkewei <zhangkewei@sina.cn>
2018-11-05 17:49:45 +0800

    EventProcessor需要实现资源初始化和销毁接口

--- porter/porter-task/src/main/java/cn/vbill/middleware/porter/task/worker/TaskWork.java
+++ porter/porter-task/src/main/java/cn/vbill/middleware/porter/task/worker/TaskWork.java
@@ -48,6 +48,7 @@ import cn.vbill.middleware.porter.task.select.SelectJob;
 import cn.vbill.middleware.porter.task.transform.TransformJob;
 import com.alibaba.fastjson.JSON;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
