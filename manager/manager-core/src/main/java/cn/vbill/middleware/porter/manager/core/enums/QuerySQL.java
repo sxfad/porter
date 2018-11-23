5f05050c9b7b8f48024084c733f6b85be3fbec5c
yunpengzhang <2305658511@qq.com>
2018-11-23 17:32:48 +0800

    mysql-connector-java版本修改为5.1.45,解决manager-boot不支持mysql5数据库问题.

--- manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/core/enums/QuerySQL.java
+++ manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/core/enums/QuerySQL.java
@@ -35,7 +35,7 @@ public enum QuerySQL {
             "SELECT table_schema AS prefixName,table_name AS tableName, CONCAT(table_schema,'.',table_name) AS tableAllName FROM information_schema.tables WHERE "
                     + "table_schema NOT IN ('test','mysql','information_schema','performance_schema','sys') ORDER BY tableAllName",
             "SELECT column_name AS fieldName FROM information_schema.COLUMNS WHERE lower(CONCAT(table_schema,'.',table_name)) = lower('%s') ORDER BY ordinal_position",
-            "com.mysql.cj.jdbc.Driver"),
+            "com.mysql.jdbc.Driver"),
 
     /**
      * ORACLE
