4fce2043ec6e7cc0457efdab25ceac9383ecbd7f
zhangkewei <zhangkewei@sina.cn>
2018-11-29 19:35:36 +0800

    全局变量SimpleDateFormat线程安全问题

--- porter/porter-core/src/main/java/cn/vbill/middleware/porter/core/event/s/converter/OggJsonConverter.java
+++ porter/porter-core/src/main/java/cn/vbill/middleware/porter/core/event/s/converter/OggJsonConverter.java
@@ -26,11 +26,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Map;
 
 /**
@@ -44,8 +43,8 @@ public class OggJsonConverter implements EventConverter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OggJsonConverter.class);
 
-    private DateFormat opTsF = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-    private DateFormat ctsf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS");
+    private FastDateFormat opTsF = FastDateFormat.getInstance("yyyy-MM-dd hh:mm:ss.SSS");
+    private FastDateFormat ctsf = FastDateFormat.getInstance("yyyy-MM-dd'T'hh:mm:ss.SSS");
 
     @Override
     public String getName() {
