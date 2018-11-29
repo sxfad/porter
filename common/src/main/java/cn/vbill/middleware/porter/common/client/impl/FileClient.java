ed3e57a11c426f48403b647a66918c695f719d77
zhangkewei <zhangkewei@sina.cn>
2018-11-29 11:28:34 +0800

    引包BUG

--- common/src/main/java/cn/vbill/middleware/porter/common/client/impl/FileClient.java
+++ common/src/main/java/cn/vbill/middleware/porter/common/client/impl/FileClient.java
@@ -34,6 +34,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author: zhangkewei[zhang_kw@suixingpay.com]
