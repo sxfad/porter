340b31a3cdd1639236b7ff87eec163d9f00bc384
zhangkewei <zhangkewei@sina.cn>
2018-12-11 12:43:27 +0800

    canal升级1.1.0

--- porter/porter-plugin/canal-consumer/src/main/java/cn/vbill/middleware/porter/plugin/consumer/canal/CanalConsumer.java
+++ porter/porter-plugin/canal-consumer/src/main/java/cn/vbill/middleware/porter/plugin/consumer/canal/CanalConsumer.java
@@ -26,6 +26,8 @@ import cn.vbill.middleware.porter.core.consumer.AbstractDataConsumer;
 import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.alibaba.otter.canal.protocol.Message;
 import cn.vbill.middleware.porter.core.event.s.MessageEvent;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,9 +46,19 @@ public class CanalConsumer extends AbstractDataConsumer {
     public List<MessageEvent> doFetch() throws TaskStopTriggerException, InterruptedException {
         return consumeClient.fetch(new ConsumeClient.FetchCallback<MessageEvent, Object>() {
             @Override
-            public <F, O> List<F> acceptAll(O o) {
+            public <F, O> List<F> acceptAll(O o) throws InvalidProtocolBufferException {
                 List<MessageEvent> events = new ArrayList<>();
                 Message msg = (Message) o;
+
+                List<CanalEntry.Entry> entries;
+                if (msg.isRaw()) {
+                    entries =  new ArrayList<>();
+                    for (ByteString e : msg.getRawEntries()) {
+                        entries.add(CanalEntry.Entry.parseFrom(e));
+                    }
+                } else {
+                    entries = msg.getEntries();
+                }
 
                 //批次消息同步提交点
                 CanalClient.CanalPosition bucketHeader = null;
@@ -61,7 +73,7 @@ public class CanalConsumer extends AbstractDataConsumer {
                     bucketHeader = new CanalClient.CanalPosition(msg.getId());
                 }
 
-                for (CanalEntry.Entry entry : msg.getEntries()) {
+                for (CanalEntry.Entry entry : entries) {
                     //事务消息同步点
                     CanalClient.CanalPosition rowHeader = new CanalClient.CanalPosition(msg.getId(), entry.getHeader().getLogfileOffset(),
                             entry.getHeader().getLogfileName());
