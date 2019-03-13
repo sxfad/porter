f5bacba76ea89c396bfc8bceabff6ebef2c734f5
Fuzz <fu_zz@Suixingpay.com>
2019-03-13 09:50:14 +0800

    数据权限控制初版

---
+++ manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/core/dto/RoleDataControl.java
@@ -0,0 +1,58 @@
+package cn.vbill.middleware.porter.manager.core.dto;
+
+import java.io.Serializable;
+
+/**
+ * 登录用户vo
+ *
+ * @author: murasakiseifu
+ * @date: 2019-03-11 10:38}
+ * @version: V1.0
+ * @review: murasakiseifu/2019-03-11 10:38
+ */
+public class RoleDataControl implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 登录用户id
+     */
+    private Long userId;
+
+    /**
+     * 登录用户角色
+     */
+    private String roleCode;
+
+    /**
+     * 登录用户id.get
+     * @return
+     */
+    public Long getUserId() {
+        return userId;
+    }
+
+    /**
+     * 登录用户id.set
+     * @param userId
+     */
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * 登录用户角色.get
+     * @return
+     */
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    /**
+     * 登录用户角色.set
+     * @param roleCode
+     */
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+}
---
+++ manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/web/rcc/RoleCheckContext.java
@@ -0,0 +1,54 @@
+package cn.vbill.middleware.porter.manager.web.rcc;
+
+
+import cn.vbill.middleware.porter.manager.core.dto.LoginUserToken;
+import cn.vbill.middleware.porter.manager.core.dto.RoleDataControl;
+import cn.vbill.middleware.porter.manager.web.token.TokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author: murasakiseifu
+ * @date: 2019-03-11 10:06}
+ * @version: V1.0
+ * @review: murasakiseifu/2019-03-11 10:06}
+ */
+public class RoleCheckContext {
+
+    protected static Logger LOGGER = LoggerFactory.getLogger(RoleCheckContext.class);
+
+    private static ThreadLocal<RoleDataControl> USERIDHOLDER = new ThreadLocal<>();
+
+    /**
+     * 判断登录用户角色当前
+     *
+     * @author FuZizheng
+     * @date 2019-03-11 10:30
+     * @param: [token]
+     * @return: void
+     */
+    public static void checkUserRole(String token) {
+        LoginUserToken loginUserToken = null;
+        try {
+            loginUserToken = TokenUtil.unsign(token, LoginUserToken.class);
+            RoleDataControl roleDataControl = new RoleDataControl();
+            roleDataControl.setUserId(loginUserToken.getUserId());
+            roleDataControl.setRoleCode(loginUserToken.getRoleCode());
+            USERIDHOLDER.set(roleDataControl);
+        } catch (Exception e) {
+            LOGGER.error("RoleCheckContext解析Token失败：[{}]", token);
+        }
+    }
+
+    /**
+     * 获取UserId
+     *
+     * @author FuZizheng
+     * @date 2019-03-11 10:30
+     * @param: []
+     * @return: java.lang.Long
+     */
+    public static RoleDataControl getUserIdHolder() {
+        return USERIDHOLDER.get();
+    }
+}
