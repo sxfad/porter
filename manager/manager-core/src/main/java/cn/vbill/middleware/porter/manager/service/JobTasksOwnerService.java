bff29c1c4ddefde7270d7e8d887ed6c66e324657
He <784753715@qq.com>
2019-03-25 16:28:28 +0800

    规范格式

--- manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/service/JobTasksOwnerService.java
+++ manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/service/JobTasksOwnerService.java
@@ -1,8 +1,9 @@
 /**
  * All rights Reserved, Designed By Suixingpay.
+ *
  * @author: FairyHood
- * @date: 2019-03-20 16:29:06  
- * @Copyright ©2017 Suixingpay. All rights reserved. 
+ * @date: 2019-03-20 16:29:06
+ * @Copyright ©2017 Suixingpay. All rights reserved.
  * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
  */
 package cn.vbill.middleware.porter.manager.service;
@@ -22,30 +23,35 @@ public interface JobTasksOwnerService {
 
     /**
      * 新增
+     *
      * @param jobTasksOwner
      */
     Integer insert(JobTasksOwner jobTasksOwner);
 
     /**
      * 修改
+     *
      * @param id, jobTasksOwner
      */
     Integer update(Long id, JobTasksOwner jobTasksOwner);
 
     /**
      * 删除
+     *
      * @param id
      */
     Integer delete(Long id);
 
     /**
      * 查询详情
+     *
      * @param id
      */
     JobTasksOwner selectById(Long id);
 
     /**
      * 分页查询
+     *
      * @param page
      */
     Page<JobTasksOwner> page(Page<JobTasksOwner> page);
--- manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/service/impl/JobTasksOwnerServiceImpl.java
+++ manager/manager-core/src/main/java/cn/vbill/middleware/porter/manager/service/impl/JobTasksOwnerServiceImpl.java
@@ -1,8 +1,9 @@
 /**
  * All rights Reserved, Designed By Suixingpay.
+ *
  * @author: FairyHood
  * @date: 2019-03-20 16:29:06
- * @Copyright ©2017 Suixingpay. All rights reserved. 
+ * @Copyright ©2017 Suixingpay. All rights reserved.
  * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
  */
 package cn.vbill.middleware.porter.manager.service.impl;
@@ -51,7 +52,7 @@ public class JobTasksOwnerServiceImpl implements JobTasksOwnerService {
     @Override
     public Page<JobTasksOwner> page(Page<JobTasksOwner> page) {
         Integer total = jobTasksOwnerMapper.pageAll(1);
-        if(total>0) {
+        if (total > 0) {
             page.setTotalItems(total);
             page.setResult(jobTasksOwnerMapper.page(page, 1));
         }
