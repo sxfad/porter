bbb74fd23848e31e19da97f1248a0521ecef6c3f
MurasakiSeiFu <fzz121014@163.com>
2019-04-26 14:12:49 +0800

    去除多余引包

--- manager/manager-boot/src/main/java/cn/vbill/middleware/porter/manager/controller/OwnerControlController.java
+++ manager/manager-boot/src/main/java/cn/vbill/middleware/porter/manager/controller/OwnerControlController.java
@@ -16,20 +16,11 @@
  */
 package cn.vbill.middleware.porter.manager.controller;
 
-import cn.vbill.middleware.porter.manager.core.entity.OwnerControl;
 import cn.vbill.middleware.porter.manager.service.OwnerControlService;
-import cn.vbill.middleware.porter.manager.web.message.ResponseMessage;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import static cn.vbill.middleware.porter.manager.web.message.ResponseMessage.ok;
 
 
 /**
